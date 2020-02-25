85635f7864d5fea81d00eb45c198fa921b79c541
Gabe <gfinch@elevensoftware.com>
2020-02-25 08:49:59 -0800

    Try to find any typos

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -16,7 +16,7 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
 
 # If the deploy succeeded, show the DNS nae of the created instance
 if [ $? -eq 0 ]; then
